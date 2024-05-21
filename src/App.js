04ad98473621c30f829bf0c1c87614b840f48793
Vadim Kuznetsov <railfuner@gmail.com>
2024-05-21 15:11:44 +0300

    added header and button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { useEffect } from 'react';
-const tg = window.Telegram.WebApp;
 
 
 function App() {
@@ -9,9 +8,6 @@ function App() {
     tg.ready();
   }, [])
 
-  const onClose = () => {
-    tg.close();
-  }
 
   return (
     <div className="App">
